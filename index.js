570894f1cd063e3b9918f980fbc4506630d3b833
infinite-void <srinathsk18@gmail.com>
2020-06-07 18:56:39 +0530

    setup pubisher-subscriber class

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+const PubSub = require('./pubsub');
+
+const app = express();
+const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+app.use(bodyParser.json());
+
+setTimeout(() => {
+    pubsub.broadcastChain();
+}, 1000); 
+
+app.get('/api/blocks', (req, res) => {
+    res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body;
+    blockchain.addBlock({ data });
+
+    res.redirect('/api/blocks');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Listening at port: ${PORT}`);
+});
